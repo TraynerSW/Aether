64ca064fb6150c87cf55c51b5c6499dca27562d5
TraynerSW <matou.vazquez@gmail.com>
2026-02-19 20:41:11 +0100

    Handle client/server disconnection and messaging

--- src/Client.java
+++ src/Client.java
@@ -1,40 +1,98 @@
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+	static boolean serverActif = false;			// Variable globale MODIFIABLE depuis les threads
+
 	public static void main(String[] args) {
 		boolean boucle = false;					// Pour afficher le "Tu es déconnecté" qu'une seule fois
-
 		Thread attente = null;
+
 		while (true) {
 			try {
-				Socket socket = new Socket("localhost", 5000);                // Si cette connexion passe, tu as le handshake.
-				if (attente != null) {			// Si le thread n'est pas encore lancé
+				Socket serverSocket = new Socket("localhost", 5000);                // Si cette connexion passe, tu as le handshake.
+				boucle = false;			// Pour afficher à nouveau le "Tu es déconnecté" si la connection se coupe.
+				serverActif = true;
+
+				if (attente != null) {				// Si le thread n'est pas encore lancé
 					attente.interrupt();
+					System.out.println("(Debug) Thread attente arrêté.");
 				}
+				String servAdr = serverSocket.getInetAddress().getHostAddress();		// Inet/Local = Son/Mon adresse
+				System.out.println("\nTu es bien connecté au serveur " + servAdr + ".\n___________________________________________________\n");
 
-				Scanner infoServ = new Scanner(socket.getInputStream());
-				String servAdr = socket.getInetAddress().getHostAddress();        // Inet = SON adresse, Local = MON adresse
-				System.out.println("\nTu es bien connecté au serveur " + servAdr + ".\n_______________________________________________________________________\n");
+				Scanner msgFromServ = new Scanner(serverSocket.getInputStream());		// Nécessaire pour écouter le serveur
+				Scanner msgUser = new Scanner(System.in);
 
-				while (true) {			// Tant qu'il est connecté, attend les messages.
-					String msgServ = infoServ.nextLine();
-					System.out.println("[Serveur] " + msgServ);
+				// Boucle de réception des messages du serveur
+				Thread receiveMsgFromServer = new Thread(() -> {
+					while (true) {
+						try {
+							if (!msgFromServ.hasNextLine()) {        // Si le serveur s'arrête
+								Thread.sleep(100);
+								System.out.println("[Serveur] Le serveur vient de se déconnecter.\n_____________________________________________________\n");
+								serverActif = false;
+								return;
+							}
+							// Récupère le message du serveur
+							String msgServ = msgFromServ.nextLine();
+							System.out.println("[Serveur] " + msgServ);
+							Thread.sleep(100);
+						} catch (Exception eReceiveMsg) {
+							System.out.println("Erreur lors de la réception d'un message du serveur :\n");
+							eReceiveMsg.printStackTrace();
+						}
+					}
+				});
+				receiveMsgFromServer.start();
 
-					Scanner usr = new Scanner(System.in);
-					String msgClient = usr.nextLine();
+				// Boucle principale d'envoi et de réception de messages (tant que le client est connecté)
+				OutputStream toServ = serverSocket.getOutputStream();	// Entrée du serveur (sortie du client)
+				PrintWriter sendServ = new PrintWriter(toServ, true);
 
+				while (true) {
+					if (serverActif) {
+						// Boucle principale d'envoi de messages au serveur
+						try {
+							// System.in.available regarde à la milliseconde près si l'utilisateur a déjà appuyé sur Entrée.
+							// > 0 : l'utilisateur a écrit un message et a appuyé sur Entrée.
+							// = 0 : le programme tourne sans être bloqué par nextLine().
+							if (System.in.available() > 0) {
+								// Message écrit de l'utilisateur
+								String msgClient = msgUser.nextLine();
+
+								// Si l'utilisateur décide de se déconnecter, arrête le programme complètement.
+								if (msgClient.equals("/exit")) {
+									System.out.println("[Client] Déconnexion en cours...");
+									System.exit(0);
+								} else {
+									if (!sendServ.checkError()) {
+										sendServ.println(msgClient);
+									} else {
+										System.out.println("Le serveur est injoignable.\n_____________________________________\n");
+										receiveMsgFromServer.interrupt();
+										//System.out.println("(Debug) Thread receiveMsgFromServer arrêté.");
+										break;
+									}
+								}
+							} else {
+								Thread.sleep(50);			// Met en pause le thread principal pour ne pas vérifier le clavier trop souvent
+							}
+						} catch (Exception eSendMsg) {
+							System.out.println("Erreur lors de l'envoi du message :\n");
+							eSendMsg.printStackTrace();
+						}
+					} else {
+						break;
+					}
 				}
 			} catch (Exception e) {
-				if (!boucle) {
-					System.out.println("Tu es déconnecté.");
-					boucle = true;
-				}
-
 				// Threads d'attente de reconnexion
 				attente = new Thread(() -> {
 					for (int i=0; i<3; i++) {
-						System.out.print("\rTentative de reconnexion");
+						System.out.print("\rTentative de connexion au serveur");
 						for (int j=0; j<4; j++) {
 							if (j > 0) {
 								System.out.print(".");
@@ -52,22 +110,31 @@ public class Client {
 					}
 				});
 
-				for (int i=6; i>=0; i--) {
+				// Boucle de reconnexion
+				if (!boucle) {
+					System.out.println("Tu es déconnecté.");
+					boucle = true;
+				}
+
+				for (int i=4; i>=0; i--) {
 					if (i==0) {
 						attente.start();
 						try {
 							Thread.sleep(4000);
-						} catch (InterruptedException ex) {
+						} catch (InterruptedException eAttente) {
 							attente.interrupt();
+							System.out.println("(Debug) Thread attente arrêté.");
+							e.printStackTrace();		// Debug
 						}
 					}
-					else if (i<6) {
+					else if (i<4) {
 						System.out.print("\rTentative de reconnexion dans " + i + " sec");
 					}
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException ex) {
 						attente.interrupt();
+						//System.out.println("(Debug) Thread attente arrêté.");
 					}
 				}
 			}
--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,13 @@
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 	public static void main (String[] args) {
 		try {
-			ServerSocket newClient = new ServerSocket(5000);
+			ServerSocket clientSocket = new ServerSocket(5000);
 
 			Thread attente = new Thread(() -> {
 				while(true) {
@@ -28,19 +28,61 @@ public class Server {
 
 			attente.start();
 			while (true) {
-				Socket client = newClient.accept();			// Attend un new client ; crée un nouveau socket et le renvoie
+				Socket client = clientSocket.accept();		// Attend un new client ; crée un nouveau socket et le renvoie
 				attente.interrupt();						// Dès qu'un client est trouvé, on stoppe le processus
 
 				String clientAdr = client.getLocalAddress().getHostAddress(); 	// Récupère l'adresse du client sans le "/"
 				int clientPort = client.getLocalPort();							// Récupère le port que le client a renseigné
 
-				System.out.print("\nUn nouveau client s'est connecté : " + clientAdr + ":" + clientPort);
+				System.out.println("\nUn nouveau client s'est connecté : " + clientAdr + ":" + clientPort);
+				OutputStream toClient = client.getOutputStream();
 
-				InputStream in = client.getInputStream();
-				OutputStream out = client.getOutputStream();
+				// Envoi de message à tous les clients connectés
+				PrintWriter msgToAllClients = new PrintWriter(toClient, true);		// True permet d'envoyer le msg direct au client connecté
+				msgToAllClients.println("Bienvenue " + client.getLocalAddress().getHostAddress() + ":" + client.getLocalPort() + " !");
 
-				PrintWriter msgServ = new PrintWriter(out, true);		// True permet d'envoyer le msg direct au client connecté
-				msgServ.println("Bienvenue " + client.getLocalAddress().getHostAddress() + ":" + client.getLocalPort() + " !");
+				Scanner msgServ = new Scanner(System.in);
+
+				Thread sendMsgToClient = new Thread(() -> {
+					while (true) {
+						try {
+							if (System.in.available() > 0) {
+								String msg = msgServ.nextLine();
+								if (msg.equals("/stop")) {
+									System.out.println("[Système] : Arrêt total du serveur en cours...");
+									System.exit(0);
+								} else if (!msg.isBlank()){			// Si que des espaces ou rien d'écrit
+									msgToAllClients.println(msg);
+								}
+							} else {
+								Thread.sleep(50);			// Met en pause le thread principal pour ne pas vérifier le clavier trop souvent
+							}
+						} catch (Exception e) {
+							System.out.println("[Système] Erreur du thread sendMsgToClient :\n");
+							e.printStackTrace();
+						}
+					}
+				});
+				sendMsgToClient.start();
+
+				// Messages du client reçus par le serveur
+				Scanner msgFromClient = new Scanner(client.getInputStream()); // Écoute le client
+
+				while (true) {
+					try {
+						if (!msgFromClient.hasNextLine()) {
+							System.out.println("Le client " + clientAdr + " s'est déconnecté.");
+							break;
+						} else {
+							String msgClient = msgFromClient.nextLine();
+							System.out.println("[Client "+ clientAdr + "] " + msgClient);
+						}
+					}
+					catch (Exception eMsgClient) {
+						System.out.println("[Système] Erreur lors de la réception du message du client " + clientAdr + " :\n");
+						eMsgClient.printStackTrace();
+					}
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
